4c053eed46523ba0679e9125edcc33f83a644f6c
DanTheStud <61593013+DanTheStud@users.noreply.github.com>
2020-03-17 19:05:42 +0200

    Add files via upload

---
+++ application.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Tests.h"
+#include "Cheltuiala.h"
+
+using namespace std;
+
+int main() 
+{
+	test_cheltuieli();
+	test_repo_cheltuieli();
+
+	int zi;
+	int sumaBani;
+	char tip[100];
+	cout << "Dati ziua: ";
+	cin >> zi;
+	cout << "Dati suma: ";
+	cin >> sumaBani;
+	cout << "Dati tipul: ";
+	cin >> tip;
+	Cheltuieli c1(zi, sumaBani, tip);
+	cout << c1;
+
+	Cheltuieli c2(c1);
+	Cheltuieli c3 = c1;
+	Cheltuieli c4;
+	c4 = c2;
+
+	return 0;
+}
